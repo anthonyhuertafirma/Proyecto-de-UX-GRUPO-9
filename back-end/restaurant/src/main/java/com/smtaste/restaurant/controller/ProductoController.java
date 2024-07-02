9a39bf08f1d12c9ba37c712b18ab701bb0174e0e
Anthony <anthony.hf1999@gmail.com>
2024-07-01 19:07:57 -0500

    Modificando carro

--- back-end/restaurant/src/main/java/com/smtaste/restaurant/controller/ProductoController.java
+++ back-end/restaurant/src/main/java/com/smtaste/restaurant/controller/ProductoController.java
@@ -30,9 +30,5 @@ public class ProductoController {
         return ResponseEntity.status(HttpStatus.OK).body(productos);
     }
     
-     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> eliminarProducto(@PathVariable Long id) {
-        productoService.eliminarProducto(id);
-        return ResponseEntity.noContent().build();
-    }
+     
 }
